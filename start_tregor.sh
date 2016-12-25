1983b9384a31305493a49d579004175b250ebb92
brelevenix <eric.debeau@gmail.com>
2016-12-25 11:23:24 +0100

    Create start_tregor.sh

---
+++ start_tregor.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Initialise VM with all required code
+# Docker, Docker-Compose, Import file
+
+# Install docker
+curl -o install.sh -sSL https://get.docker.com/
+sudo sh install.sh
+sudo groupadd docker
+sudo gpasswd -a cloud docker
+sudo service docker restart
+echo "docker installed and running"
+
+# Install docker-compose
+sudo apt-get -y install python-pip
+sudo pip install docker-compose
+echo "docker-compose installed"
+
+# Import OSM data
+cd ~
+sudo mkdir osm
+cd osm
+sudo wget http://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf
+sudo mv bretagne-latest.osm.pbf import.osm.pbf
+
+echo "OSM data imported"
+
+# Launch the containers
+cd ~
+sudo wget https://raw.githubusercontent.com/brelevenix/OpenStreetMap-Tileserver-DockerCompose/master/docker-compose.yml
+sudo docker-compose up -d
+echo "containers running"
