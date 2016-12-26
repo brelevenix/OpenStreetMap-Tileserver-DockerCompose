5b6ac0eba403d8c74ccfc0cff55d66ba06baa7e7
brelevenix <eric.debeau@gmail.com>
2016-12-26 08:38:36 +0100

    Update start_tregor.sh

--- start_tregor.sh
+++ start_tregor.sh
@@ -20,12 +20,18 @@ cd ~
 sudo mkdir osm
 cd osm
 sudo wget http://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf
-sudo mv bretagne-latest.osm.pbf import.osm.pbf
-
+sudo mv bretagne-latest.osm.pbf zone.osm.pbf
 echo "OSM data imported"
+
+# Extract zone
+sudo wget https://raw.githubusercontent.com/brelevenix/osm-tregor-data/master/22113.poly
+sudo mv 22113.poly zone.poly
+sudo apt-get install osmosis -y
+sudo osmosis --read-pbf file=zone.osm.pbf --bounding-polygon file=zone.poly --write-pbf file=import.osm.pbf
+echo "OSM data extracted"
 
 # Launch the containers
 cd ~
-sudo wget https://raw.githubusercontent.com/brelevenix/OpenStreetMap-Tileserver-DockerCompose/master/docker-compose.yml
+sudo wget -O docker-compose.yml https://raw.githubusercontent.com/brelevenix/OpenStreetMap-Tileserver-DockerCompose/master/docker-compose.yml
 sudo docker-compose up -d
 echo "containers running"
